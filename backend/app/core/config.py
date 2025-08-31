9be25792e2950fe768d153e8b0e2c888298cbc10
iskandar boukhenoufa <iskandar@iskandar.iskandar.com>
2025-08-31 12:19:42 +0100

    Initial project commit

---
+++ backend/app/core/config.py
@@ -0,0 +1,30 @@
+# FILE: app/core/config.py
+
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+	APP_NAME: str = "ResumeAPI"
+	APP_ENV: str = "development"
+	SECRET_KEY: str
+	ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+	REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 7 days
+
+	# Database
+	DB_HOST: str
+	DB_PORT: int
+	DB_USER: str
+	DB_PASSWORD: str
+	DB_NAME: str
+
+	# Email Config
+	EMAIL_HOST: str
+	EMAIL_PORT: int
+	EMAIL_USERNAME: str
+	EMAIL_PASSWORD: str
+
+	PROJECT_DOMAIN: str
+
+	class Config:
+		env_file = ".env"
+
+settings = Settings()
---
+++ backend/app/schemas/user.py
@@ -0,0 +1,17 @@
+from pydantic import BaseMode, EmailStr
+
+
+class UserCreate(BaseModel):
+	email: EmailStr
+	password: str
+
+
+class UserOut(BaseModel):
+	id: int
+	email: EmailStr
+	is_active: bool
+
+	class Config:
+		orm_mode: True
+
+
